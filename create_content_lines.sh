f3b82bc70947456d13903d11d8d080cc3db33d63
Daniel Biegler <DanielBiegler@users.noreply.github.com>
2018-08-02 20:17:27 +0200

    add pdf and content bash script

---
+++ create_content_lines.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+if [ $# -lt 5 ]; then
+	echo "You need 5 arguments: start_line, end_line, /path/genome.fna, fold-width, /path/output"
+	exit 1
+fi
+
+# $1: Start line, inclusive
+# $2: End line, inclusive
+# $3: Path to the DNA Data in fasta file format
+# $4: Fold width to properly fit the page
+# $5: Path to output content file
+sed -n "$1,$2p;$(($2+1))q" $3 | grep -v '^>' | tr -d '\nNn' | fold -w $4 | sed 's/^/<text:p text:style-name="P1">/' | sed 's/$/<\/text:p>/' > $5
---
+++ make_pdf.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# 1. /path/master.odt
+# 2. /path/sequence_content.txt
+# 3. /path/output.odt
+if [ $# -lt 3 ]; then
+	echo "You need 3 arguments: /path/master.odt, /path/sequence_content.txt, /path/output.odt"
+	exit 1
+fi
+
+echo "Creating content.xml"
+# unpack to stdout	  # replace text with REPLACEME on seperate line     # replace with file contents
+unzip -p $1 content.xml | perl -p -e 's/<text:p.*?(?=<\/o)/\nREPLACEME\n/' | sed -e "/REPLACEME/{r $2" -e 'd}' > content.xml
+
+echo "Copying $1 to $3"
+cp $1 $3
+
+echo "Adding content.xml to $3"
+zip $3 content.xml
+
+echo "Remove content.xml"
+rm content.xml
+
+echo "Creating PDF. This can take a while"
+libreoffice --convert-to "pdf" $3
