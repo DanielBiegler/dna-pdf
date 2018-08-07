0b997b74321318199b13448c3e45f2893dd38bff
Daniel Biegler <DanielBiegler@users.noreply.github.com>
2018-08-07 14:29:12 +0200

    add script for creating all content files

---
+++ create.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+bash create_content_lines.sh 1 3121254 hidden/GRCh38_latest_genomic.fna 268 hidden/content_01.txt
+bash create_content_lines.sh 35376151 36024385 hidden/GRCh38_latest_genomic.fna 268 hidden/content_22.txt
+bash create_content_lines.sh 34792275 35376151 hidden/GRCh38_latest_genomic.fna 268 hidden/content_21.txt
+bash create_content_lines.sh 33986721 34792275 hidden/GRCh38_latest_genomic.fna 268 hidden/content_20.txt
+bash create_content_lines.sh 33253999 33986721 hidden/GRCh38_latest_genomic.fna 268 hidden/content_19.txt
+bash create_content_lines.sh 32249331 33253999 hidden/GRCh38_latest_genomic.fna 268 hidden/content_18.txt
+bash create_content_lines.sh 31201370 32249331 hidden/GRCh38_latest_genomic.fna 268 hidden/content_17.txt
+bash create_content_lines.sh 30048728 31201370 hidden/GRCh38_latest_genomic.fna 268 hidden/content_16.txt
+bash create_content_lines.sh 28768232 30048728 hidden/GRCh38_latest_genomic.fna 268 hidden/content_15.txt
+bash create_content_lines.sh 27416514 28768232 hidden/GRCh38_latest_genomic.fna 268 hidden/content_14.txt
+bash create_content_lines.sh 25986958 27416514 hidden/GRCh38_latest_genomic.fna 268 hidden/content_13.txt
+bash create_content_lines.sh 24321015 25986958 hidden/GRCh38_latest_genomic.fna 268 hidden/content_12.txt
+bash create_content_lines.sh 22631176 24321015 hidden/GRCh38_latest_genomic.fna 268 hidden/content_11.txt
+bash create_content_lines.sh 20958707 22631176 hidden/GRCh38_latest_genomic.fna 268 hidden/content_10.txt
+bash create_content_lines.sh 19225091 20958707 hidden/GRCh38_latest_genomic.fna 268 hidden/content_09.txt
+bash create_content_lines.sh 17410857 19225091 hidden/GRCh38_latest_genomic.fna 268 hidden/content_08.txt
+bash create_content_lines.sh 15419031 17410857 hidden/GRCh38_latest_genomic.fna 268 hidden/content_07.txt
+bash create_content_lines.sh 13283955 15419031 hidden/GRCh38_latest_genomic.fna 268 hidden/content_06.txt
+bash create_content_lines.sh 11013565 13283955 hidden/GRCh38_latest_genomic.fna 268 hidden/content_05.txt
+bash create_content_lines.sh 8633259 11013565 hidden/GRCh38_latest_genomic.fna 268 hidden/content_04.txt
+bash create_content_lines.sh 6152619 8633259 hidden/GRCh38_latest_genomic.fna 268 hidden/content_03.txt
+bash create_content_lines.sh 3121254 6152619 hidden/GRCh38_latest_genomic.fna 268 hidden/content_02.txt
+bash create_content_lines.sh 36024385 37974898 hidden/GRCh38_latest_genomic.fna 268 hidden/content_X.txt
+bash create_content_lines.sh 37974898 40717176 hidden/GRCh38_latest_genomic.fna 268 hidden/content_Y.txt
